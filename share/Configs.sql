3ce3f75d1592768cb0da320118d71c05ea7fc7f3
oscisn93 <ocisneros1@csu.fullerton.edu>
2023-10-07 20:57:37 -0700

    chagned case

--- share/Configs.sql
+++
@@ -1,8 +0,0 @@
-PRAGMA foreign_keys = ON;
-BEGIN TRANSACTION;
-DROP TABLE IF EXISTS configs;
-CREATE TABLE configs (
-  automatic_enrollment INTEGER NOT NULL
-);
-INSERT INTO configs (automatic_enrollment) VALUES (1);
-COMMIT;
